2019f23199a06181b076026503d3fea268f6e3d1
NatasFX <nischmitt@inf.ufsm.br>
2023-06-14 16:16:46 -0300

    Added utils.py

---
+++ NatasTF/utils.py
@@ -0,0 +1,16 @@
+# from NatasTF.parser import code
+import sys
+
+
+def print_error_line(lexpos):
+    code = "".join(open(sys.argv[1], 'rb').read().decode('utf8'))
+    line_code = code.split('\n')
+    for idx, line in enumerate(line_code):
+        if lexpos < len(line):
+            print('Linha', idx+1, '|', line)
+            break
+        else:
+            lexpos -= len(line) + 1     # +1 por causa do newline
+        
+    print('-'*(len('Linha '+str(idx+1)+' |')+lexpos+1),end='')
+    print('^')
